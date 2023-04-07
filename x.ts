1aea7be2e6ceedd1bab35ec54e931613160842dd
restuwahyu13 <restuwahyu13@gmail.com>
2023-04-07 22:03:17 +0700

    Publish files

--- x.ts
+++
@@ -1,7 +0,0 @@
-import { config } from 'dotenv'
-config({ path: './.env' })
-
-const filename = 'payload.php\x00.jpg'
-const blacklistMime: boolean = new RegExp(`(${process.env.FILE_BLACKLIST})`, 'gi').test(filename)
-
-console.log('File Injection Ext Detected: ', blacklistMime)
